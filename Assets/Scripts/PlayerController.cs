9042cdea5395ad0d27579bdaa922ccbf1b17c791
flexusISF <146860748+flexusISF@users.noreply.github.com>
2023-11-08 10:20:19 -0800

    Unit 4 Final day 3
    Got all the scripts finished just need to clean up the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRb;
-    public float speed = 5.0f;
+    public float speed = 2.0f;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    public bool isOnGround = true;
+    public float jumpForce;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -18,9 +24,15 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Horizontal");
         playerRb.AddForce(Vector2.right * speed * forwardInput);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector2.up * 100, ForceMode2D.Impulse);
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = true;
         }
+        
+        
+    }private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
